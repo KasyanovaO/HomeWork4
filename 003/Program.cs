27f0d81f81ffa1a386e16bea07114da312cf126a
OK <@mail.ru>
2022-08-18 14:58:00 +0300

    add 003

---
+++ 003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[8] { 6, 1, 33, 5, 14, 7, 8, 45 };
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i == array.Length - 1) 
+            {
+                Console.Write(array[i]+"");
+            }
+            else 
+            {
+                Console.Write(array[i]+", ");  
+            }
+        }
+        Console.Write("]");
+    }
+}
